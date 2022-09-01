564a1dcdccde5ad90452260ee0a4bf1733401efd
CzerstfyChlep <35424664+CzerstfyChlep@users.noreply.github.com>
2022-09-01 18:17:39 +0200

    Add project files.

---
+++ Force Decode Device/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Force_Decode_Device
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.ForceButton = new System.Windows.Forms.Button();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Code = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 12);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(260, 85);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.Text = "Input";
+            // 
+            // ForceButton
+            // 
+            this.ForceButton.Location = new System.Drawing.Point(12, 103);
+            this.ForceButton.Name = "ForceButton";
+            this.ForceButton.Size = new System.Drawing.Size(260, 29);
+            this.ForceButton.TabIndex = 1;
+            this.ForceButton.Text = "Use the FORCE, Luke!";
+            this.ForceButton.UseVisualStyleBackColor = true;
+            this.ForceButton.Click += new System.EventHandler(this.ForceButton_Click);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(12, 138);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(260, 30);
+            this.OutputBox.TabIndex = 2;
+            this.OutputBox.Text = "Output";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Code used:";
+            // 
+            // Code
+            // 
+            this.Code.AutoSize = true;
+            this.Code.Location = new System.Drawing.Point(79, 181);
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(0, 13);
+            this.Code.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(200, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Spacebar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 203);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Code);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.ForceButton);
+            this.Controls.Add(this.InputBox);
+            this.Name = "Form1";
+            this.Text = "Force Decode Device";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button ForceButton;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Code;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Force Decode Device/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Force_Decode_Device
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {           
+            InitializeComponent();            
+        }
+        public string[] Alphabet = { "A","Ą", "B", "C","Ć", "D", "E","Ę", "F", "G", "H", "I", "J", "K", "L","Ł", "M", "N","Ń","O","Ó","P", "Q", "R", "S", "Ś","T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "/", "?", "<", ">", ",", ".", ":", ";", "|", "{", "}", "[", "]" , "a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó", "p", "r", "s", "ś", "t","u","v","w","x","y","z","ż","ź", "`", "~", "€", " ", "\"", "\'", "\\", "¡", "¢", "£", "¤", "¥", "¦", "§", "¨", "©", "ª", "«", "»", "¬", "°", "±", "²", "³", "´", "µ", "¶", "·", "¸", "¹", "º", "¼", "½", "À", "Á", "Â"
+,"Ã","Ä","Æ","Ç","È","É","Ê","Ë","Ì","Í","Ð","Ñ","Ò","Ô","Õ","Ö","×","Ø","Ù","Ú","Û","Ü"};
+        public string[] CodeAlphabet = { "A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "/", "?", "<", ">", ",", ".", ":", ";", "|", "{", "}", "[", "]", "a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó", "p", "r", "s", "ś", "t", "u", "v", "w", "x", "y", "z", "ż", "ź", "`", "~", "€", " ", "\"", "\'", "\\", "¡", "¢", "£", "¤", "¥", "¦", "§", "¨", "©", "ª", "«", "»", "¬", "°", "±", "²", "³", "´", "µ", "¶", "·", "¸", "¹", "º", "¼", "½", "À", "Á", "Â"
+,"Ã" ,"Ä","Æ","Ç","È","É","Ê","Ë","Ì","Í","Ð","Ñ","Ò","Ô","Õ","Ö","×","Ø","Ù","Ú","Û","Ü"};
+        public int LetterTry = 0;
+        public int SpinsTry = 1;
+        public bool Reverse1 = false;
+        public void MoveLetters(int RotationNumber = 1)
+        {
+            for (int N = RotationNumber; N != 0; N--)
+            {
+                Queue CodeAlphabetQ = new Queue(CodeAlphabet);
+                CodeAlphabetQ.Enqueue(CodeAlphabetQ.Dequeue());
+                CodeAlphabetQ.CopyTo(CodeAlphabet, 0);
+            }
+        }
+        public void SetCodeAlphabet()
+        {
+            if (Reverse1 == false)
+            {
+                Alphabet.CopyTo(CodeAlphabet, 0);
+                int Position = LetterTry;
+                for (; Position > 0; Position--)
+                {
+                    MoveLetters();
+                }
+            }
+            else
+            {
+                Alphabet.CopyTo(CodeAlphabet, 0);
+                Array.Reverse(CodeAlphabet);
+                int Position = LetterTry;
+                for (; Position > 0; Position--)
+                {
+                    MoveLetters();
+                }
+            }
+        }
+        private void ForceButton_Click(object sender, EventArgs e)
+        {
+            Loop:
+            SetCodeAlphabet();
+            string Text = InputBox.Text;
+            char[] Arr = Text.ToCharArray();
+            Text = "";
+            foreach (char c in Arr)
+            {
+                MoveLetters(SpinsTry);       
+                try
+                {
+                    int Index = Array.IndexOf(CodeAlphabet, c.ToString());
+                    Text += Alphabet.ElementAt(Index);
+                }
+                catch
+                {
+                }
+
+            }
+            if (LetterTry < 164)
+            {
+                LetterTry++;
+            }
+            else
+            {
+                LetterTry = 0;
+                if (SpinsTry < 164)
+                {
+                    SpinsTry++;
+                }
+                else
+                {
+                    SpinsTry = 1;
+                    Reverse1 = true;
+                }
+            }
+            if (checkBox1.Checked == true)
+            {
+                if (Text.Contains(" ") == true && (Text.Contains("ª") == false && Text.Contains("ª") == false && Text.Contains("¬") == false && Text.Contains("À") == false && Text.Contains("Á") == false && Text.Contains("Ã") == false && Text.Contains("Ä") == false && Text.Contains("Æ") == false && Text.Contains("Ç") == false && Text.Contains("È") == false && Text.Contains("É") == false && Text.Contains("Ê") == false && Text.Contains("Ë") == false && Text.Contains("Ì") == false && Text.Contains("Í") == false && Text.Contains("Ð") == false && Text.Contains("Ñ") == false && Text.Contains("Ò") == false && Text.Contains("Ô") == false && Text.Contains("Õ") == false && Text.Contains("Ö") == false && Text.Contains("Ø") == false && Text.Contains("Ù") == false && Text.Contains("Ú") == false && Text.Contains("Û") == false && Text.Contains("Ü") == false && Text.Contains("½") == false && Text.Contains("¼") == false && Text.Contains("³") == false && Text.Contains("²") == false && Text.Contains("±") == false && Text.Contains("°") == false && Text.Contains("¤") == false && Text.Contains("¨") == false && Text.Contains("{") == false && Text.Contains("}") == false && Text.Contains("|") == false && Text.Contains("©") == false && Text.Contains("_") == false && Text.Contains("#") == false && Text.Contains("^") == false && Text.Contains("~") == false && Text.Contains("`") == false && Text.Contains("\'") == false && Text.Contains("¡") == false && Text.Contains("¢") == false && Text.Contains("¥") == false && Text.Contains("¦") == false && Text.Contains("§") == false && Text.Contains("«") == false && Text.Contains("»") == false && Text.Contains("°") == false && Text.Contains("µ") == false && Text.Contains("¶") == false && Text.Contains("·") == false && Text.Contains("¸") == false && Text.Contains("¹") == false))
+                {
+                    goto Result;
+                }
+            }
+            else
+            {
+                if ((Text.Contains("ª") == false && Text.Contains("ª") == false && Text.Contains("¬") == false && Text.Contains("À") == false && Text.Contains("Á") == false && Text.Contains("Ã") == false && Text.Contains("Ä") == false && Text.Contains("Æ") == false && Text.Contains("Ç") == false && Text.Contains("È") == false && Text.Contains("É") == false && Text.Contains("Ê") == false && Text.Contains("Ë") == false && Text.Contains("Ì") == false && Text.Contains("Í") == false && Text.Contains("Ð") == false && Text.Contains("Ñ") == false && Text.Contains("Ò") == false && Text.Contains("Ô") == false && Text.Contains("Õ") == false && Text.Contains("Ö") == false && Text.Contains("Ø") == false && Text.Contains("Ù") == false && Text.Contains("Ú") == false && Text.Contains("Û") == false && Text.Contains("Ü") == false && Text.Contains("½") == false && Text.Contains("¼") == false && Text.Contains("³") == false && Text.Contains("²") == false && Text.Contains("±") == false && Text.Contains("°") == false && Text.Contains("¤") == false && Text.Contains("¨") == false && Text.Contains("{") == false && Text.Contains("}") == false && Text.Contains("|") == false && Text.Contains("©") == false && Text.Contains("_") == false && Text.Contains("#") == false && Text.Contains("^") == false && Text.Contains("~") == false && Text.Contains("`") == false && Text.Contains("\'") == false && Text.Contains("¡") == false && Text.Contains("¢") == false && Text.Contains("¥") == false && Text.Contains("¦") == false && Text.Contains("§") == false && Text.Contains("«") == false && Text.Contains("»") == false && Text.Contains("°") == false && Text.Contains("µ") == false && Text.Contains("¶") == false && Text.Contains("·") == false && Text.Contains("¸") == false && Text.Contains("¹") == false))
+                {
+                    goto Result;
+                }
+            }
+            goto Loop;
+            Result:
+            DialogResult result = MessageBox.Show(Text, "Confirmation", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.Yes)
+            {
+                OutputBox.Text = Text;
+                Code.Text = Alphabet.GetValue((LetterTry - 1)) + " " + SpinsTry + " " + Reverse1;
+                LetterTry = 0;
+                SpinsTry = 0;
+                Reverse1 = false;
+            }
+            else if (result == DialogResult.No)
+            {
+                goto Loop;
+            }
+            else
+            {
+                LetterTry = 0;
+                SpinsTry = 0;
+                Reverse1 = false;
+
+            }
+        }
+    }
+    
+}
+//(Text.Contains(" się ") == true || Text.Contains(" i ") == true || Text.Contains(" w ") == true || Text.Contains("nie") == true || Text.Contains("na") == true || Text.Contains(" z ") == true || Text.Contains("do") == true || Text.Contains("to") == true || Text.Contains(" że ") == true || Text.Contains(" a ") == true || Text.Contains(" o ") == true || Text.Contains(" jak ") == true || Text.Contains("ale") == true || Text.Contains("po") == true || Text.Contains("co") == true || Text.Contains("tak") == true || Text.Contains("za") == true || Text.Contains("od") == true || Text.Contains("go") == true || Text.Contains("już") == true || Text.Contains("jego") == true && Text.Contains("jego") == true && Text.Contains("jego") == true && Text.Contains("jego") == true && Text.Contains("jego") == true)  || 
